892f94a6cb2a027ffdd3e39b3b1f4c2dbb4a2d83
KRQuinn <kr.quinn82@gmail.com>
2021-10-29 16:00:10 -0700

    working on actions endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
 // Write your "actions" router here!
+const router = require("express").Router()
+
+const Actions = require('./actions-model')
+
+router.get('/', (req, res, next) => {
+    Actions.get(req.params.id)
+      .then((action) => {
+        res.status(200).json(action)
+      })
+      .catch(next)
+  })
+
+//eslint-disable-next-line
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      customMessage: 'This server will self destruct in 5 seconds. Have a nice day!',
+      message: err.message,
+    })
+  })
+module.exports = router
